19d967104b733e3b17c8ee909bfa33d09dcb0014
jayounghoyos <jayoungh@eafit.edu.co>
2023-09-12 11:08:34 -0500

    "primiera version"

---
+++ script3.js
@@ -0,0 +1,4 @@
+const tipoUrbanizacion = document.querySelector("#urbanizacion");
+const zonas = document.querySelector(".zonas");
+
+tipoUrbanizacion.addEventListener("select", () => { zonas.remov })
